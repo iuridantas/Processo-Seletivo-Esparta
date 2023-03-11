b1cc6b08ae7f47118d197a03494aad874804fca3
Iuri Dantas <uri.dantas@hotmail.com>
2023-03-11 18:19:17 -0300

    feat: created base structure tasks

--- project-management-plan/src/app.module.ts
+++ project-management-plan/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './prisma/database.module';
 import { ProjectModule } from './project/project.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [DatabaseModule,ProjectModule],
+  imports: [DatabaseModule,ProjectModule, TasksModule],
 })
 export class AppModule {}
